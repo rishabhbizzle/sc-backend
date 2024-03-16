0e707e11552e29bc845b0799207f843a7c7039c8
rishabhbizzle <rishabhbizzle@gmail.com>
2024-03-17 00:52:20 +0530

    added a test origin in cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 app.use(express.json());
 app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    origin: ['http://localhost:3000', 'https://spotracker.tech', 'https://www.spotracker.tech']
+    origin: ['http://localhost:3000', 'https://spotracker.tech', 'https://www.spotracker.tech', 'https://statforfans.netlify.app', 'https://www.statforfans.netlify.app']
   }))
 
 async function connect() {
